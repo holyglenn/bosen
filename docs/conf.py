fd194f7b668513f693e2a2d1a1d41f1bfb6bda34
DaiWei <daiwei89@gmail.com>
2016-07-08 19:59:47 -0400

    try doxygen

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,38 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
+
+
+
+# hack for readthedocs to cause it to run doxygen first
+# https://github.com/rtfd/readthedocs.org/issues/388
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    from subprocess import call 
+      call('doxygen')
+
+# -- General configuration
+# ------------------------------------------------
+
+# If your documentation needs a minimal Sphinx version, state it here.
+#needs_sphinx = '1.0'
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'breathe',
+    ]
+
+# Breathe extension variables
+breathe_projects = { "Bosen-Breathe": "doxyxml/" }
+breathe_default_project = "Bosen-Breathe"
 
 # -- General configuration ------------------------------------------------
 
@@ -29,7 +58,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+# extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
